7f1cb85a33aa1bbc41c5dd19f7a49ac0eed4349e
aowuchengzi <1533971306@qq.com>
2018-10-09 15:27:17 +0800

    update

--- test.py
+++ test.py
@@ -21,10 +21,11 @@ mask = array([[[0., 1.],
                [0., 1.]]])
 new_mask = np.zeros((3, 3) + (2, ))
 new_mask
-
+mask == 0
+mask[1,1]=3
 #%%
-for i in range(3):
+for i in range(2):
    
     new_mask[mask == i,i] = 1
-new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
+new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) 
 mask = new_mask
